3aacded4b42b50b02f550773a391861f5fa41fbc
Jhon Cabrera <jhoncabrera2050@gmail.com>
2022-11-10 15:24:43 -0500

    commit cabrera

--- src/main/java/com/tecsup/petclinic/repositories/OwnerRepository.java
+++ src/main/java/com/tecsup/petclinic/repositories/OwnerRepository.java
@@ -1,5 +1,21 @@
+
+
 package com.tecsup.petclinic.repositories;
 
-public interface OwnerRepository {
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tecsup.petclinic.entities.Owner;
+
+@Repository
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    List<Owner> findByName(String name);
+
+    List<Owner> findByTypeId(int typeId);
+
+    List<Owner> findByOwnerId(int ownerId);
 
 }
--- src/test/java/com/tecsup/petclinic/services/OwnerServiceBusquedaTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceBusquedaTest.java
@@ -1,5 +1,6 @@
 package com.tecsup.petclinic.services;
 
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 

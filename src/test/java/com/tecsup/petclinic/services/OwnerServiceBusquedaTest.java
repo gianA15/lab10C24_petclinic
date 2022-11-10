e368056520fcce23734c50560cc19edb683e1776
Abraham Guzman <85902876+abrahamguzman@users.noreply.github.com>
2022-11-10 15:41:55 -0500

    Commit 9 -> mguzman

--- src/test/java/com/tecsup/petclinic/services/OwnerServiceBusquedaTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceBusquedaTest.java
@@ -1,24 +1,37 @@
 package com.tecsup.petclinic.services;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import com.tecsup.petclinic.entities.Owner;
 import com.tecsup.petclinic.exception.OwnerNotFoundException;
 
+
+@SpringBootTest
 public class OwnerServiceBusquedaTest {
-	private static final Logger logger = LoggerFactory.getLogger(OwnerServiceTest.class);
+	
+	private static final Logger logger = LoggerFactory.getLogger(OwnerServiceBusquedaTest.class);
 
-	//@Autowired
-   //	private OwnerService ownerService;
-
+	@Autowired
+	private OwnerService ownerService;
+	
 
 	@Test
 	public void testFindOwnerById() {
 
 		long ID = 1;
-		String NAME = "Jaime";
+		String first_name = "George";
 		Owner owner = null;
 		
 		try {
@@ -30,7 +43,7 @@ public class OwnerServiceBusquedaTest {
 		}
 		logger.info("" + owner);
 
-		assertEquals(NAME, owner.getName());
+		assertEquals(first_name, owner.getName());
 
 	}
 }
